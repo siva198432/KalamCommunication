cf854dc85869cb4ff15a386549cce8defc905b69
siva198432 <59727265+siva198432@users.noreply.github.com>
2020-01-12 18:42:24 +0200

    Update Registration.php

--- resource/Registration.php
+++ resource/Registration.php
@@ -1,9 +1,9 @@
 <html>
 <body>
 
-First Name <?php echo $_GET["firstname"]; ?><br>
-Las Name is: <?php echo $_GET["lastname"]; ?>
-Mobile: <?php echo $_GET["mobilenumber"]; ?>
+First Name <?php echo $_GET["studentname"]; ?><br>
+Las Name is: <?php echo $_GET["emailid"]; ?>
+Mobile: <?php echo $_GET["mobileno"]; ?>
 
 </body>
 </html>
